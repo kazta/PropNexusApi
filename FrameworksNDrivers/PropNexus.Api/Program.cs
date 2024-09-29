166c907179fb1a686140eaa3ea6eb42f989bae99
Joan Sebastian Castañeda Correa <joan.castaneda@tecnofactory.com.co>
2024-09-29 16:17:32 -0500

    Add inject on program.cs and add getallpropertiescontroller

--- FrameworksNDrivers/PropNexus.Api/Program.cs
+++ FrameworksNDrivers/PropNexus.Api/Program.cs
@@ -1,6 +1,9 @@
+using PropNexus.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDependencies();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
