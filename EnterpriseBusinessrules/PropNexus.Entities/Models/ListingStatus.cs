b95a0830f72c5863488d30a36948927d431e8062
JOAN SEBASTIAN CASTAÑEDA CORREA <jscastaneda@serviasesorias.com.co>
2024-09-29 19:11:56 -0500

    Add missing models

---
+++ EnterpriseBusinessrules/PropNexus.Entities/Models/ListingStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace PropNexus.Entities.Models;
+public class ListingStatus
+{
+    public long Id { get; set; }
+    public string StatusName { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
