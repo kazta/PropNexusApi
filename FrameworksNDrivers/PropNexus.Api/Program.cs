eed6363a40e32f037ff03e94a774bc89bafe21bc
JOAN SEBASTIAN CASTAÑEDA CORREA <jscastaneda@serviasesorias.com.co>
2024-10-01 15:11:27 -0500

    Lock swagger try in relese mode

--- FrameworksNDrivers/PropNexus.Api/Program.cs
+++ FrameworksNDrivers/PropNexus.Api/Program.cs
@@ -13,11 +13,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (!app.Environment.IsDevelopment())
+    {
+        c.DefaultModelsExpandDepth(-1);
+        c.SupportedSubmitMethods([]);
+    }
+});
 
 app.UseHttpsRedirection();
 
